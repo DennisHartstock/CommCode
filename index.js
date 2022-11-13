facb00201d55f61308dcae0b585044d85f25165d
DennsHartstock <dennis.hartstock@web.de>
2022-11-13 15:10:32 +0100

    update skill levels

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const deButton = document.getElementById("de")
-
-deButton.addEventListener('click', function handleClick(){
-deButton.innerHTML = '<span style="background-color: red">'
-})
+function de() {
+    document.getElementById("about_me_p1").innerText = "Mein Name is Dennis"
+    document.getElementById("about_me_p2").innerText = "p2"
+    document.getElementById("about_me_p3").innerText = "p3"
+}
