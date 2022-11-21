8ee6b1728782070d014a9c587dd1e207e74bf7ee
DennsHartstock <dennis.hartstock@web.de>
2022-11-21 16:35:47 +0100

    several changes in translation mode

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function de() {
-    document.getElementById("about_me_p1").innerText = "Mein Name is Dennis"
-    document.getElementById("about_me_p2").innerText = "p2"
-    document.getElementById("about_me_p3").innerText = "p3"
-}
+// function ru() {
+//     document.getElementById("about_me_p1").innerText = "Меня зовут Денис. Я увлёкся программированием в 2020 году, \
+//     когда из-за пандемии коронавируса весь наш мир погрузился в пучину неопределённости и алогичности.\
+//     \n\nЧуть менее чем за год прошёл полный курс изучения Java на Javarush и потренировался на тренажёрах HTML Academy, \
+//     чтобы получить начальные представления о фронтенде и сверстать эту страницу.\
+//     \n\nНа данный момент я занимаюсь разработкой мобильных приложений в Android Studio, потому что мне нравится \
+//     создавать вещи, которые могут приносить пользу другим людям."
+// }
+
+// function de() {
+//     document.getElementById("about_me_p1").innerText = "Mein Name ist Dennis"
+// }
+
+// function en() {
+//     document.getElementById("about_me_p1").innerText = "My Name is Dennis"
+// }
+
+// function es() {
+//     document.getElementById("about_me_p1").innerText = "Me llamo Dennis"
+// }
