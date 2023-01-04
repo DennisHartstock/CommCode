01b4e4452c3d9729595357e6368c50ab4053cc99
DennsHartstock <dennis.hartstock@web.de>
2023-01-04 12:00:41 +0100

    add content translations

--- index.js
+++ index.js
@@ -1,20 +1,99 @@
-// function ru() {
-//     document.getElementById("about_me_p1").innerText = "Меня зовут Денис. Я увлёкся программированием в 2020 году, \
-//     когда из-за пандемии коронавируса весь наш мир погрузился в пучину неопределённости и алогичности.\
-//     \n\nЧуть менее чем за год прошёл полный курс изучения Java на Javarush и потренировался на тренажёрах HTML Academy, \
-//     чтобы получить начальные представления о фронтенде и сверстать эту страницу.\
-//     \n\nНа данный момент я занимаюсь разработкой мобильных приложений в Android Studio, потому что мне нравится \
-//     создавать вещи, которые могут приносить пользу другим людям."
-// }
+function ru() {
+    document.getElementById("about_me_p1").innerText = "Меня зовут Денис. Я увлёкся программированием в 2020 году, \
+    когда из-за пандемии коронавируса весь наш мир погрузился в пучину неопределённости и алогичности."
 
-// function de() {
-//     document.getElementById("about_me_p1").innerText = "Mein Name ist Dennis"
-// }
+    document.getElementById("about_me_p2").innerText = "Чуть менее чем за год прошёл полный курс изучения Java на JAVARUSH \
+    и потренировался на тренажёрах HTML Academy, чтобы получить начальные представления о фронтенде и сверстать эту страницу."
 
-// function en() {
-//     document.getElementById("about_me_p1").innerText = "My Name is Dennis"
-// }
+    document.getElementById("about_me_p3").innerText = "На данный момент я занимаюсь разработкой мобильных приложений \
+    в Android Studio, потому что мне нравится создавать вещи, которые могут приносить пользу другим людям."
 
-// function es() {
-//     document.getElementById("about_me_p1").innerText = "Me llamo Dennis"
-// }
+    document.getElementById("about_me_p4").innerText = "На этой странице вы найдёте различные ресурсы, которые показались мне \
+    полезными в процессе изучения программирования."
+
+    document.getElementById("about_me_p5").innerText = "Помимо этого я буду выкладывать здесь свои учебные проекты и мобильные приложения."
+
+    document.getElementById("skills").innerText = "Мои навыки программирования \n \
+    (в затраченных на обучение часах)"
+
+    document.getElementById("res").innerText = "Полезные ресурсы"
+
+    document.getElementById("my_apps").innerText = "Мобильные приложения"
+
+    document.getElementById("projects").innerText = "Учебные проекты"
+}
+
+function de() {
+    document.getElementById("about_me_p1").innerText = "Mein Name ist Dennis. Ich bin 2020 zum Programmieren gekommen, \
+    als unsere gesamte Welt aufgrund der Coronavirus-Pandemie in einen Abgrund der Unsicherheit und Unlogik gestürzt ist."
+
+    document.getElementById("about_me_p2").innerText = "In etwas weniger als einem Jahr habe ich einen vollständigen Java-Kurs zu JAVARUSH abgeschlossen \
+    und an den Simulatoren der HTML Academy geübt, um ein grundlegendes Verständnis des Frontends und des Layouts dieser Seite zu erlangen."
+
+    document.getElementById("about_me_p3").innerText = "Ich entwickle derzeit mobile Anwendungen \
+    in Android Studio, weil ich gerne Dinge erstelle, von denen andere profitieren können."
+
+    document.getElementById("about_me_p4").innerText = "Auf dieser Seite finden Sie verschiedene Ressourcen, die mir \
+    nützlich beim Programmieren lernen."
+
+    document.getElementById("about_me_p5").innerText = "Außerdem werde ich hier meine Bildungsprojekte und mobilen Anwendungen veröffentlichen."
+
+    document.getElementById("skills").innerText = "Meine Programmierkenntnisse \n\
+    (in Trainingsstunden)"
+
+    document.getElementById("res").innerText = "Nützliche Ressourcen"
+
+    document.getElementById("my_apps").innerText = "Mobile Anwendungen"
+
+    document.getElementById("projects").innerText = "Bildungsprojekte"
+}
+
+function en() {
+    document.getElementById("about_me_p1").innerText = "My name is Dennis. I got into programming in 2020, \
+    when, due to the coronavirus pandemic, our entire world plunged into an abyss of uncertainty and illogicality."
+
+    document.getElementById("about_me_p2").innerText = "In a little less than a year, I completed a full Java course on JAVARUSH \
+    and practiced on the HTML Academy simulators to get a basic understanding of the frontend and layout this page."
+
+    document.getElementById("about_me_p3").innerText = "I am currently developing mobile applications \
+    in Android Studio because I enjoy creating things that other people can benefit from."
+
+    document.getElementById("about_me_p4").innerText = "On this page you will find various resources that seemed to me \
+    useful in the process of learning programming."
+
+    document.getElementById("about_me_p5").innerText = "In addition, I will post here my educational projects and mobile applications."
+
+    document.getElementById("skills").innerText = "My programming skills \n\
+    (in hours spent on training)"
+
+    document.getElementById("res").innerText = "Useful Resources"
+
+    document.getElementById("my_apps").innerText = "Mobile applications"
+
+    document.getElementById("projects").innerText = "Educational projects"
+}
+
+function es() {
+    document.getElementById("about_me_p1").innerText = "Mi nombre es Dennis. Entré en la programación en 2020, \
+    cuando, debido a la pandemia del coronavirus, nuestro mundo entero se hundió en un abismo de incertidumbre e ilógica."
+
+    document.getElementById("about_me_p2").innerText = "En poco menos de un año, completé un curso completo de Java sobre JAVARUSH \
+    y practicó en los simuladores de HTML Academy para obtener una comprensión básica de la interfaz y el diseño de esta página."
+
+    document.getElementById("about_me_p3").innerText = "Actualmente estoy desarrollando aplicaciones móviles \
+    en Android Studio porque disfruto creando cosas de las que otras personas pueden beneficiarse."
+
+    document.getElementById("about_me_p4").innerText = "En esta página encontrarás varios recursos que me parecieron \
+    útil en el proceso de aprendizaje de la programación."
+
+    document.getElementById("about_me_p5").innerText = "Además, publicaré aquí mis proyectos educativos y aplicaciones móviles."
+
+    document.getElementById("skills").innerText = "Mis habilidades de programación \n\
+    (en horas dedicadas a la formación)"
+
+    document.getElementById("res").innerText = "Recursos útiles"
+
+    document.getElementById("my_apps").innerText = "Aplicaciones móviles"
+
+    document.getElementById("projects").innerText = "Proyectos educativos"
+}
